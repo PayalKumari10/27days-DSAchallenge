a26783bec75d7967f97d85d08ff17dc7e54bca51
PayalKumari10 <kumaripayal7488@gmail.com>
2025-05-04 15:07:01 +0530

    Add program to find second smallest and second largest element

--- Arrays/Find_Second_Smallest_and_Second_Largest_Element.java
+++ Arrays/Find_Second_Smallest_and_Second_Largest_Element.java
@@ -1,4 +1,39 @@
-import java.util.Arrays;
+// import java.util.Arrays;
+
+// class ArrayUtils {
+//     //Bruteforce approach to find second smallest and second largest element in an array
+//     //Time complexity: O(nlogn) due to sorting the array
+//     //Space complexity: O(1) as we are not using any extra space
+//     public static void getElements(int[] arr, int nums) {
+//         if(nums == 0 || nums == 1) {
+//             System.out.println(-1);
+//             System.out.println("");
+//             System.out.println(-1);
+//             System.out.println("\n");
+//             return;
+//         }
+
+//         Arrays.sort(arr);
+//         int small = arr[1];
+//         int large = arr[nums-2];
+//         System.out.println("Second smallest is: " + small);
+//         System.out.println("Second largest is: " + large);
+//     }
+
+//     public static void main(String args[]) {
+//         int[] arr = {1, 2, 4, 6, 7, 5};
+//         int n = arr.length;
+//         getElements(arr, n);
+//     }
+// }
+
+
+
+//Best approach to find second smallest and second largest element in an array
+//Time complexity: O(n) as we are traversing the array only once
+//Space complexity: O(1) as we are not using any extra space
+
+
 
 class ArrayUtils {
     //Bruteforce approach to find second smallest and second largest element in an array
@@ -12,12 +47,26 @@ class ArrayUtils {
             System.out.println("\n");
             return;
         }
+        int small = Integer.MAX_VALUE;
+        int large = Integer.MIN_VALUE;
+        int second_Small = Integer.MAX_VALUE;
+        int second_Large = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < nums; i++) {
+           small = Math.min(small, arr[i]);
+              large = Math.max(large, arr[i]);
+        }
+        for(int i = 0; i < nums; i++) {
+            if(arr[i] != small) {
+                second_Small = Math.min(second_Small, arr[i]);
+            }
+            if(arr[i] != large) {
+                second_Large = Math.max(second_Large, arr[i]);
+            }
+        }
+        System.out.println("Second smallest is: " + second_Small);
+        System.out.println("Second largest is: " + second_Large);
 
-        Arrays.sort(arr);
-        int small = arr[1];
-        int large = arr[nums-2];
-        System.out.println("Second smallest is: " + small);
-        System.out.println("Second largest is: " + large);
     }
 
     public static void main(String args[]) {
@@ -26,10 +75,3 @@ class ArrayUtils {
         getElements(arr, n);
     }
 }
-
-
-
-//Best approach to find second smallest and second largest element in an array
-//Time complexity: O(n) as we are traversing the array only once
-//Space complexity: O(1) as we are not using any extra space
-
